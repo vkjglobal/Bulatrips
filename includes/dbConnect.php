bee1de6e2d1aecf36e506a6cd4001c70f6175b6a
Nafees Khan Lodhi <nafeesdroidor@gmail.com>
2024-11-18 10:12:23 +0500

    changes and coonflicts resolved and ready to deploye

--- includes/dbConnect.php
+++ includes/dbConnect.php
@@ -45,3 +45,16 @@ if ($_SERVER['HTTP_HOST'] == 'localhost:8080') {
         exit("Error: " . $e->getMessage());
     }
 }
+
+else if ($_SERVER['HTTP_HOST'] == 'staging.bulatrips.com') {
+    define('DB_HOST', 'localhost'); // Host name
+    define('DB_USER', 'bulatrips_staging'); // db user name
+    define('DB_PASS', '@]9E~IwT7k%L'); // db user password name
+    define('DB_NAME', 'bulatrips_staging'); // db name
+    // Establish database connection.
+    try {
+        $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
+}

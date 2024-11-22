643caac55915cd191780ef345d41890ab4414bc1
Asad Ali Qureshi <adqureshi@gmail.com>
2024-11-22 22:33:27 +0500

    updated CRON

--- CronJob/cronSearchPtr.php
+++ CronJob/cronSearchPtr.php
@@ -12,222 +12,143 @@ Project Name	::> Bulatrips
 ********************/
 
 include_once __DIR__ . '/../includes/class.SearchPtrCron.php';
+include_once('/../includes/class.Users.php');
 include_once __DIR__ . '/../includes/common_const.php';
 include_once __DIR__ . '/../mail_send.php';
 
- $objBookCron     =   new SearchPtrCron();
-  $adminToemail  =   "no-reply@bulatrips.com";
+$objBookCron     =   new SearchPtrCron();
+$adminToemail  =   "no-reply@bulatrips.com";
 
   //=================log write for book API ======
-                   
-                   //$logRes =   print_r($responseData, true);
-                 // $logReQ =   print_r($requestData, true);
-                 $logReQ =   "Successfully started";
-   
-                    $objBookCron->_writeLog('-------------'.date('l jS \of F Y h:i:s A').'-------------','searchPtrCron.txt');
-                    $objBookCron->_writeLog('Request Received\n'.$logReQ,'searchPtrCron.txt');
-                            
-             // echo "Log updated successfully";exit; 
-    //============ END log write for book API ==========
+$logReQ =   "Successfully started";
+$objBookCron->_writeLog('-------------'.date('l jS \of F Y h:i:s A').'-------------','searchPtrCron.txt');
+$objBookCron->_writeLog('Request Received\n'.$logReQ,'searchPtrCron.txt');
+//============ END log write for book API ==========
+$resultBooking = $objBookCron->getBookCronIDs();
+foreach($resultBooking as $resultBookingdata){
 
-    // Subscribe the user and get the result message
-    $resultBooking = $objBookCron->getBookCronIDs();
-
-     // echo "<pre/>";print_r($resultBooking);exit;
-    foreach($resultBooking as $resultBookingdata){
-
-        // print_r($resultBookingdata);
-        $userId =    $resultBookingdata['user_agent_id'];
-        $ptr_id =   $resultBookingdata['ptr_id'];
-        $mfreNum = $resultBookingdata['mf_ref_num'];
-        $totalRefundAmount = $resultBookingdata['total_refund_amount'];
-        if(isset($mfreNum)){
-           // $apiEndpoint = 'https://restapidemo.myfarebox.com/api/v1.1/TripDetails/{MFRef}';
-           // $bearerToken = '18AEA8F0-5B21-41ED-9993-DD7A8123B0D2-1560';
-           if(isset($resultBookingdata['ptr_type']) == "Refund"){
-                    $requestData = array(
-                        'ptrType' => 'Refund',
-                        'MFRef' => $mfreNum,
-                        'PTRId' => $ptr_id,
-                        'Page' => 1
-                   );
-            }
-            else{
+    $userId =    $resultBookingdata['user_agent_id'];
+    $ptr_id =   $resultBookingdata['ptr_id'];
+    $mfreNum = $resultBookingdata['mf_ref_num'];
+    $totalRefundAmount = $resultBookingdata['total_refund_amount'];
+    if(isset($mfreNum)){
+        if(isset($resultBookingdata['ptr_type']) == "Refund"){
                 $requestData = array(
-                    'ptrType' => 'Void',
+                    'ptrType' => 'Refund',
                     'MFRef' => $mfreNum,
                     'PTRId' => $ptr_id,
                     'Page' => 1
-               );
-            }
-            $endpoint   =   'Search/PostTicketingRequest';
-            $result       =   $objCancel->callApi($endpoint,$requestData);
-            $httpCode = $result['httpCode'];
-            $response = $result['responseData'];
+                );
+        }
+        else{
+            $requestData = array(
+                'ptrType' => 'Void',
+                'MFRef' => $mfreNum,
+                'PTRId' => $ptr_id,
+                'Page' => 1
+            );
+        }
+        $endpoint   =   'Search/PostTicketingRequest';
+        $result       =   $objCancel->callApi($endpoint,$requestData);
+        $httpCode = $result['httpCode'];
+        $response = $result['responseData'];
 
-            if ($response) {
-                $responseData = json_decode($response, true);
-        
-            }
-            $logRes =   print_r($responseData, true);
-            $logReQ =   print_r($requestData, true);
+        if ($response) {
+            $responseData = json_decode($response, true);
     
-                $objCancel->_writeLog('-------------'.date('l jS \of F Y h:i:s A').'-------------','search.txt');
-                            $objCancel->_writeLog('userId is '.$userId,'search.txt');
-                          $objCancel->_writeLog('Booking ID is '.$bookingId,'search.txt');
-                          $objCancel->_writeLog('Request Received\n'.$logReQ,'search.txt');
-    
-                        $objCancel->_writeLog('REsponse Received for MF:\n'.$mfreNum,'search.txt');
-    
-            $objCancel->_writeLog('REsponse Received\n'.$logRes,'search.txt');
-     
-                            //write log
-        // print_r($responseData);exit;
-            //=====================================
-         // $traceId    =    $responseData['Data']['TraceId'];
-          $precancelsts   =   'post';
-         $message = ""; 
-          $TotalRefundAmount =0;
-          $CreditNoteNumber ='';
-          $CreditNoteStatus ='';
-          $Resolution   =   '';
-          if(isset($responseData['Success'])){
+        }
+        $logRes =   print_r($responseData, true);
+        $logReQ =   print_r($requestData, true);
+
+        $objCancel->_writeLog('-------------'.date('l jS \of F Y h:i:s A').'-------------','search.txt');
+        $objCancel->_writeLog('userId is '.$userId,'search.txt');
+        $objCancel->_writeLog('Booking ID is '.$bookingId,'search.txt');
+        $objCancel->_writeLog('Request Received\n'.$logReQ,'search.txt');
+
+        $objCancel->_writeLog('REsponse Received for MF:\n'.$mfreNum,'search.txt');
+
+        $objCancel->_writeLog('REsponse Received\n'.$logRes,'search.txt');
+        $message = ""; 
+        $TotalRefundAmount =0;
+        $CreditNoteNumber ='';
+        $CreditNoteStatus ='';
+        $Resolution   =   '';
+        if(isset($responseData['Success'])){
             //  echo  $responseData['Data']['PTRDetail']['PTRId'];
-    if (isset($responseData['Data']['PTRDetail']) && (!empty($responseData['Data']['PTRDetail']))) {
-      //  print_r($responseData);exit;
+            if (isset($responseData['Data']['PTRDetail']) && (!empty($responseData['Data']['PTRDetail']))) {
                 $cancel_status  =   0;
-                 $PTRId    =   $responseData['Data']['PTRDetail'][0]['PTRId'];
-                $PTRType    =   $responseData['Data']['PTRDetail'][0]['PTRType'];
-                $BookingStatus   =   $responseData['Data']['PTRDetail'][0]['BookingStatus'];
-                $PTRStatus      =   $responseData['Data']['PTRDetail'][0]['PTRStatus'];     
-                
-                $Resolution   =   $responseData['Data']['PTRDetail'][0]['Resolution'];
-                $ProcessingMethod   =   $responseData['Data']['PTRDetail'][0]['ProcessingMethod'];
-                $CreditNoteNumber   =   $responseData['Data']['PTRDetail'][0]['CreditNoteNumber'];
-                $CreditNoteStatus   =   $responseData['Data']['PTRDetail'][0]['CreditNoteStatus'];
-                 $TotalRefundAmount  +=   $responseData['Data']['PTRDetail'][0]['TotalRefundAmount'];
-                 $Currency   =   $responseData['Data']['PTRDetail'][0]['Currency'];
-    
+                $PTRId    =   $responseData['Data']['PTRDetail'][0]['PTRId'];
+            $PTRType    =   $responseData['Data']['PTRDetail'][0]['PTRType'];
+            $BookingStatus   =   $responseData['Data']['PTRDetail'][0]['BookingStatus'];
+            $PTRStatus      =   $responseData['Data']['PTRDetail'][0]['PTRStatus'];     
+            
+            $Resolution   =   $responseData['Data']['PTRDetail'][0]['Resolution'];
+            $ProcessingMethod   =   $responseData['Data']['PTRDetail'][0]['ProcessingMethod'];
+            $CreditNoteNumber   =   $responseData['Data']['PTRDetail'][0]['CreditNoteNumber'];
+            $CreditNoteStatus   =   $responseData['Data']['PTRDetail'][0]['CreditNoteStatus'];
+                $TotalRefundAmount  +=   $responseData['Data']['PTRDetail'][0]['TotalRefundAmount'];
+                $Currency   =   $responseData['Data']['PTRDetail'][0]['Currency'];
+            
                 $objCancel->_writeLog('Step 1Success '.$PTRStatus,'search.txt');
-                 $objCancel->_writeLog('Step 1 Resolution '.$Resolution,'search.txt');
-    
-                  $success_can_sts    =0;
+                $objCancel->_writeLog('Step 1 Resolution '.$Resolution,'search.txt');
+
+                $success_can_sts    =0;
                 foreach($responseData['Data']['PTRDetail'][0]['pTRPaxDetails'] as $k => $val){
-                     $pax_booking_id_transaction =   $val['Id'];
-                     $PaxId =   $val['PaxId'];
-                     $TicketStatus =   $val['TicketStatus'];
-                     $is_active_booking_status =   $val['IsActive'];                
+                    $pax_booking_id_transaction =   $val['Id'];
+                    $PaxId =   $val['PaxId'];
+                    $TicketStatus =   $val['TicketStatus'];
+                    $is_active_booking_status =   $val['IsActive'];                
                     $ticket_num =   $val['TicketNumber']; 
                 
                     if(($PTRStatus == "Completed") && ($Resolution == "Voided")){
                         //cancellation success
-                         
-                          $update_cancelBooking_status      =    $objCancel->updateInDB_cancelbooking('cancel_booking',$ticket_num);
-                           $update_TravellerB_result      =    $objCancel->updateInDB_trav('travellers_details',$ticket_num);
-                          $cancel_status    =1;
+                        
+                        $update_cancelBooking_status      =    $objCancel->updateInDB_cancelbooking('cancel_booking',$ticket_num);
+                        $update_TravellerB_result      =    $objCancel->updateInDB_trav('travellers_details',$ticket_num);
+                        $cancel_status    =1;
     
                     }
-                   
-                   $bookCanIns      =   $objCancel->insCncelSts_Search($bookingId,$userId,$BookingStatus,$Resolution, $mfreNum,$ProcessingMethod,$PTRId,$PTRType,$CreditNoteNumber,$PTRStatus,$CreditNoteStatus, $ticket_num ,$pax_booking_id_transaction ,$PaxId,$TicketStatus,$TotalRefundAmount,$is_active_booking_status,$cancel_status);                                                   
-                                      
-                                 
-                                              
-                                                         
+                
+                $bookCanIns      =   $objCancel->insCncelSts_Search($bookingId,$userId,$BookingStatus,$Resolution, $mfreNum,$ProcessingMethod,$PTRId,$PTRType,$CreditNoteNumber,$PTRStatus,$CreditNoteStatus, $ticket_num ,$pax_booking_id_transaction ,$PaxId,$TicketStatus,$TotalRefundAmount,$is_active_booking_status,$cancel_status);                                                   
+                                    
+                                
+                                            
+                                                        
     
                 }
-                       $count_ticketed_temp    =   $objCancel->count_ticketed__temp_book('travellers_details',$bookingId);
-                        if($count_ticketed_temp == 0){
-                            //  update tempbooking and traveller details tables with cancelled sts 
-                         $update_tempB_result           =   $objCancel->updateInDB_temp_book('temp_booking',$mfreNum);
-                       }
-                   //    echo "LLL";print_r($TotalRefundAmount)  ;exit;
-    //mail code
-                                             
-                //   var_dump($update_TravellerB_result);exit;
+                $count_ticketed_temp    =   $objCancel->count_ticketed__temp_book('travellers_details',$bookingId);
+                if($count_ticketed_temp == 0){
+                    //  update tempbooking and traveller details tables with cancelled sts 
+                    $update_tempB_result           =   $objCancel->updateInDB_temp_book('temp_booking',$mfreNum);
+                }
                 if($cancel_status == 1){
-                                 $message   =   "Your Canellation is :".$PTRStatus." Total Refundable Amount is : ". $Currency." ".$TotalRefundAmount."Please note Your CreditNoteNumber: ".$CreditNoteNumber;
-    
+                    $message   =   "Your Canellation is :".$PTRStatus." Total Refundable Amount is : ". $Currency." ".$TotalRefundAmount."Please note Your CreditNoteNumber: ".$CreditNoteNumber;
                 }
                 else{
                     $message   =   "Your Canellation is :".$PTRStatus." Total Refundable Amount is : ". $Currency." ".$TotalRefundAmount;
                 }
-             
-    
-                                  $response_New = array(
-                        'status' => 'success', // You can set this to 'error' in case of an error
-                        'message' => $message,
-                        'ptr_id' => $PTRId,
-                        'refundamount' => $TotalRefundAmount
-                    );
-                   // print_r($response_New);exit;
-          }
-          else if(isset($responseData['Data']['Errors']) && is_array($responseData['Data']['Errors'])) {
-         // print_r($responseData['Data']['Errors']);exit;
-        foreach ($responseData['Data']['Errors'] as $error) {
-            $errorCode = $error['Code'];
-            $errorMessage = $error['Message'];
-           $cancel_status = 0;
-           $message_new = $errorMessage;
-                             //    $bookCanIns      =   $objCancel->insCncelSts($bookingId,$userId,$precancelsts,$errorCode ='', $mfreNum,$traceId='',$httpCode,$PTRType='',$SLAInMinutes='',$PTRStatus='',$VoidingWindow='', $ticket_num=''  ,$AdminCharges='' ,$GSTCharge='',$TotalVoidingFee='',$TotalRefundAmount='',$Currency='',$cancel_status,$message_new);                                                   
-    
-             //  $bookCanIns      =   $objCancel->insCncelSts($bookingId,$userId,$precancelsts,$errorCode,$mfreNum,$traceId,$httpCode,$cancel_status);
-               //echo $errorCode;exit;
-            $message    = "Problem in Cancellation";
-              $cancel_status = 0;
-                             $response_New = array(
-                'status' => 'error', // You can set this to 'error' in case of an error
-                'message' => $message
-            );
-                           $bookCanIns      =   $objCancel->insCncelSts_Search($bookingId,$userId,$BookingStatus='',$Resolution='', $mfreNum,$ProcessingMethod='',$PTRId='',$PTRType='',$CreditNoteNumber='',$PTRStatus='',$CreditNoteStatus='', $ticket_num='' ,$pax_booking_id_transaction='' ,$PaxId='',$TicketStatus='',$TotalRefundAmount='',$is_active_booking_status='',$cancel_status='',$message);                                                   
-    
-             $objCancel->_writeLog('Step data of errors  '.$errorMessage,'search.txt');
-     
-                }
-               
-            } //== end of if error ===
-            else if($httpCode !=200)
-            {
-                  $cancel_status = 0;
-                // Handle other status codes like 404, 500, etc.
-                $message =  "API request failed with status code: " . $httpCode;
-                $message_new    = $message;
-                   $bookCanIns      =   $objCancel->insCncelSts_Search($bookingId,$userId,$BookingStatus='',$Resolution='', $mfreNum,$ProcessingMethod='',$PTRId='',$PTRType='',$CreditNoteNumber='',$PTRStatus='',$CreditNoteStatus='', $ticket_num='' ,$pax_booking_id_transaction='' ,$PaxId='',$TicketStatus='',$TotalRefundAmount='',$is_active_booking_status='',$cancel_status='',$message);                                                   
-             
-                             $response_New = array(
-                'status' => 'error', // You can set this to 'error' in case of an error
-                'message' => $message
-            );
-             $objCancel->_writeLog('step httpcode not 200 '.$message,'search.txt');
+                $objUser = new Users();
+                $userDetails = $objUser->getUserDetails($userId);
+                $subject = "Bulatrips Agent Booking and Credit Balance Debit Info";                  
+
+                        $email=   $userDetails['email'];
+                        $name   =   $userDetails['first_name']." ".$userDetails['last_name'];
+                        $content    =   '<p>Hello,</p>
+                                            <p>This agent , '. $name .', with email '.$userDetails['email'].' ptr request against booking id:'.$tempBookingId.'.The booking is response has been return as completed</p>';
+                                        $messageData =   $objBook->getEmailContent($content);
+                    // print_r($messageData);exit;
+                        $headers="";
+                        $email = $adminToemail; //Need ADMIN email here
+        
+                    $contacts= sendMail($email,$subject, $messageData,$headers);
             }
-            else if(empty($responseData['Data'])){
-                if(!empty($responseData['Message'])){   // echo "uu";exit;
-                               
-                                  $cancel_status = 0;
-                   
-                            $message    =   $responseData['Message'];
-                             $message_new    = $message;
-                   $bookCanIns      =   $objCancel->insCncelSts_Search($bookingId,$userId,$BookingStatus='',$Resolution='', $mfreNum,$ProcessingMethod='',$PTRId='',$PTRType='',$CreditNoteNumber='',$PTRStatus='',$CreditNoteStatus='', $ticket_num='' ,$pax_booking_id_transaction='' ,$PaxId='',$TicketStatus='',$TotalRefundAmount='',$is_active_booking_status='',$cancel_status='',$message);                                                   
-    
-                            //may alreay cancelled 
-                             $response_New = array(
-                                'status' => 'error', // You can set this to 'error' in case of an error
-                                'message' => $message
-                            );
-                                          $objCancel->_writeLog('step data empty '.$message,'search.txt');
-    
-                }
-    
-    
-            } 
-          }
-          
-             $objCancel->_writeLog('step end of void  ========= '.$message,'search.txt');
-            echo json_encode($response_New);
-            exit;
-    
-           
-          
         }
+        
+        $objCancel->_writeLog('step end of void  ========= '.$message,'search.txt');
+
+        
+        
     }
+}
     
 ?>
--- includes/class.SearchPtrCron.php
+++ includes/class.SearchPtrCron.php
@@ -109,86 +109,66 @@ include_once __DIR__ . '/../includes/class.Db_clientCron.php';
                 return null;
             }
     }
-    public function getUsersbyId($user_id) {
-        try {
-            $stmt = $this->conn->prepare("SELECT * FROM users WHERE id = :user_id");
-            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-            $stmt->execute();
-
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            return $result;
-        } catch (PDOException $e) {
-            // Handle the exception (e.g., log the error)
-            return null;
-        }
-    }
-     public function getUserDetails($user_id) {
-        $result = $this->getUsersbyId($user_id);
-        return $result;
-    }
-    
     public function getEmailContent($content){
+        $messageDatacontent =   $content;
+             $messageData      = '
+            <html>
+            <body>
+            <table style="width:100%">
+                <tbody>
+                    <tr>
+                        <td>
+                            <center>
+                                <table style="width:80%;margin:0 auto">
 
-                            $messageDatacontent =   $content;
-                                 $messageData      = '
-                                <html>
-                                <body>
-                                <table style="width:100%">
                                     <tbody>
                                         <tr>
-                                            <td>
-                                                <center>
-                                                    <table style="width:80%;margin:0 auto">
-    
-                                                        <tbody>
-                                                            <tr>
-                                                                <td style="text-align:center;padding-bottom:15px;padding-top:15px">
-                                                                    <h2 style="margin-top:0;margin-bottom:0">
-                                                                        <img width="125" height="30"
-                                                                            src="https://bulatrips.com/images/bulatrips-logo.png"
-                                                                            alt="Bulatrip" title="Bulatrip"
-                                                                            style="height:30px;width:125px;display:inline-block;margin-top:0;margin-bottom:0"
-                                                                            class="CToWUd" data-bit="iit">
-                                                                    </h2>
-                                                                </td>
-                                                            </tr>
-    
-    
-                                                            <tr>
-                                                                <td bgcolor="#ffffff" style="padding-top:20px;text-align:center">
-    
-                                                                    <div width="100%"
-                                                                        style="max-width:480px;padding:5pt 0;background-color:#eff5fc;border-radius:10px;margin:0 auto;width:calc(100% - 32px);margin-bottom:24px">
-    
-    
-    
-                                               
-    
-                                                                    </div>
-    
-    
-    
-                                                                    <div align="center" style="padding:0 10px;padding-bottom:5px">
-                                                                        <p
-                                                                            style="font-family:Arial,sans-serif;color:#000000;letter-spacing:-0.5px;text-align:center;margin-top:0;margin-bottom:0">
-                                                                            '.$messageDatacontent.'
-                                                                        </p>
-                                                                    </div>
-                                                                </td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
-                                                </center>
+                                            <td style="text-align:center;padding-bottom:15px;padding-top:15px">
+                                                <h2 style="margin-top:0;margin-bottom:0">
+                                                    <img width="125" height="30"
+                                                        src="https://bulatrips.com/images/bulatrips-logo.png"
+                                                        alt="Bulatrip" title="Bulatrip"
+                                                        style="height:30px;width:125px;display:inline-block;margin-top:0;margin-bottom:0"
+                                                        class="CToWUd" data-bit="iit">
+                                                </h2>
+                                            </td>
+                                        </tr>
+
+
+                                        <tr>
+                                            <td bgcolor="#ffffff" style="padding-top:20px;text-align:center">
+
+                                                <div width="100%"
+                                                    style="max-width:480px;padding:5pt 0;background-color:#eff5fc;border-radius:10px;margin:0 auto;width:calc(100% - 32px);margin-bottom:24px">
+
+
+
+                           
+
+                                                </div>
+
+
+
+                                                <div align="center" style="padding:0 10px;padding-bottom:5px">
+                                                    <p
+                                                        style="font-family:Arial,sans-serif;color:#000000;letter-spacing:-0.5px;text-align:center;margin-top:0;margin-bottom:0">
+                                                        '.$messageDatacontent.'
+                                                    </p>
+                                                </div>
                                             </td>
                                         </tr>
                                     </tbody>
                                 </table>
-                             </body>
-                                </html>';
+                            </center>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+         </body>
+        </html>';
 
-                                return   $messageData;
-     }
-
+        return   $messageData;
+    }
 }
 
 ?>
